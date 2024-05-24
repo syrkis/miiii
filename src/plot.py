c9538ce36e36681b6dec62d41fd92645ae209ef6
Noah Syrkis <noah@syrkis.com>
2024-05-24 19:32:36 -0300

    add spiral plots

--- src/plot.py
+++ src/plot.py
@@ -5,18 +5,30 @@
 # imports
 import plotly as py
 import pandas as pd
+import jax.numpy as jnp
 import numpy as np
+import darkdetect
 from datetime import date, timedelta
 import tikz
 import matplotlib.pyplot as plt
 
 
 def polar_fn(v):  # maps v to a polar plot
-    # map each point n in v to (n, n * theta)
-    theta = 2 * np.pi / len(v)
-    coords = [(n, n * theta) for n in v]
-    fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
-    ax.plot(coords)
+    ink = "black" if darkdetect.isLight() else "white"
+    bg = "white" if darkdetect.isLight() else "black"
+    scale = jnp.log(v.size)
+    fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=(14, 14))
+    ax.grid(False)
+    # set color to ink
+    ax.scatter(v, v, color=ink, s=10 / scale)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    # set colors to ink and bg
+    ax.set_facecolor(bg)
+    fig.patch.set_facecolor(bg)
+    plt.rcParams["text.color"] = ink
+    # tight layout
+    plt.tight_layout()
     plt.show()
 
 
@@ -24,32 +36,6 @@ def hilbert_fn(v):  # maps v to a hilbert curve
     pass
 
 
-# tikz functions
-def plot_fn():
-    pic = tikz.Picture()
-    # mlp in -> activation -> mlp out
-    pic.draw("(-1, 0) rectangle (1, 1)")
-    pic.draw("(-1, 0.5) -- (1, 0.5)", "dashed")
-    pic.draw("(-1, 0.5) circle (0.5)")
-    pic.draw("(-1, 0.5) -- (-1, 1.5)", "dashed")
-    pic.draw("(1, 0.5) -- (1, 1.5)", "dashed")
-    pic.draw("(-1, 1.5) rectangle (1, 2.5)")
-    pic.draw("(-1, 2) -- (1, 2)", "dashed")
-    pic.draw("(0, 2) circle (0.5)")
-    pic.draw("(0, 2) -- (0, 3)", "dashed")
-    pic.draw("(0, 3) rectangle (1, 4)")
-    pic.draw("(0, 3.5) -- (1, 3.5)", "dashed")
-    pic.draw("(0, 3.5) circle (0.5)")
-    pic.draw("(0, 3.5) -- (0, 4.5)", "dashed")
-    pic.draw("(0, 4.5) rectangle (1, 5.5)")
-    # save
-    pic.write("mlp.tikz")
-    # save as png
-
-
-def main():
-    plot_fn()
-
-
 if __name__ == "__main__":
-    main()
+    v = jnp.arange(1000)
+    polar_fn(v)
