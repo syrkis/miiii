9abccee3352d2af93e5d1b836da91bec921f9c6a
Noah Syrkis <noah@syrkis.com>
2024-06-16 15:39:30 -0300

    log acc and f1 to wandb

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import miiii
 
 
 # functions
-def log_run(conf, metrics):
+def log_run(cfg, metrics):
     # long loss and epoch
     log_fn = lambda x: {
         "train_loss": x[0],
@@ -21,26 +21,26 @@ def log_run(conf, metrics):
         "train_f1": x[4],
         "valid_f1": x[5],
     }
-    with wandb.init(project="miiii", config=conf):
+    with wandb.init(project="miiii", config=cfg):
         for epoch, metric in enumerate(metrics[:-1]):
             wandb.log(log_fn(metric), step=epoch, commit=False)
-        wandb.log(log_fn(metrics[-1]), step=conf.epochs)
+        wandb.log(log_fn(metrics[-1]), step=cfg.epochs)
         # TODO: log images and model, and maybe more
 
 
 def main():
     # config and init
-    conf, (rng, key) = miiii.get_conf(), random.split(random.PRNGKey(0))
-    data = miiii.prime_fn(conf.n, partial(miiii.base_n, conf.base))
-    params = miiii.init_fn(key, conf)
+    cfg, (rng, key) = miiii.get_conf(), random.split(random.PRNGKey(0))
+    data = miiii.prime_fn(cfg.n, partial(miiii.base_n, cfg.base))
+    params = miiii.init_fn(key, cfg)
 
     # train
     apply_fn = miiii.make_apply_fn(miiii.vaswani_fn)
-    train_fn, opt_state = miiii.init_train(apply_fn, params, conf, *data)
-    (params, opt_state), metrics = train_fn(conf.epochs, rng, (params, opt_state))
+    train_fn, opt_state = miiii.init_train(apply_fn, params, cfg, *data)
+    (params, opt_state), metrics = train_fn(cfg.epochs, rng, (params, opt_state))
 
     # evaluate
-    log_run(conf, metrics)  # log run
+    log_run(cfg, metrics)  # log run
 
 
 if __name__ == "__main__":
