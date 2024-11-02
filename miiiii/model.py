41ac8ef999528751aef27be7544a9bc0cc58a395
Noah Syrkis <noah@syrkis.com>
2024-11-02 17:39:04 +0100

    moving to tripper

--- miiiii/model.py
+++ miiiii/model.py
@@ -68,7 +68,7 @@ class Params:
 @dataclass
 class Output:
     logits: Array
-    acts: Tuple[Activation, Activation]
+    activation: Tuple[Activation, Activation]
     embeds: Array
 
 
@@ -78,9 +78,9 @@ def apply_fn(cfg: Conf):
     def apply(p: Params, rng: Array, x: Array, dropout: float) -> Output:
         embeds = embed_fn(p.embeds, x)
         step_fn = partial(block_fn, dropout=dropout)
-        z, acts = lax.scan(step_fn, embeds, (key_fn(p, rng), p.blocks))
+        z, activation = lax.scan(step_fn, embeds, (key_fn(p, rng), p.blocks))
         logits = (z @ p.unbeds).sum(axis=0)
-        return Output(logits=logits, acts=acts, embeds=embeds)
+        return Output(logits=logits, activation=activation, embeds=embeds)
 
     return apply
 
@@ -106,7 +106,7 @@ def attn_fn(p: Attention, x: Array):
 
 def ffwd_fn(p: Feedforward, x: Array) -> Tuple[Array, Array]:
     z = jnp.dot(x, p.w1)  # + p.b1  # z: seq_len x emb_dim
-    z = jax.nn.tanh(z)  # grokfast tanh to things are around 0
+    z = jax.nn.relu(z)  # grokfast tanh to things are around 0
     return z @ p.w2, z  # + p.b2  # disable biases as per @nanda2023
 
 
@@ -125,22 +125,22 @@ def layer_norm(params: LayerNorm, x: Array) -> Array:
 # %% Initializers ###########################################################
 def init_embed_fn(rng: Array, cfg: Conf):
     keys = random.split(rng, 2)
-    tok_emb = init_array(keys[0], (cfg.prime, cfg.hyper.latent_dim))  # type: ignore
-    pos_emb = init_array(keys[1], (2, cfg.hyper.latent_dim))  # type: ignore
+    tok_emb = init_array(keys[0], (cfg.prime, cfg.latent_dim))  # type: ignore
+    pos_emb = init_array(keys[1], (2, cfg.latent_dim))  # type: ignore
     return Embedding(tok_emb=tok_emb, pos_emb=pos_emb)
 
 
 def init_attn_fn(rng: Array, cfg: Conf) -> Attention:
     keys = random.split(rng, 4)
-    shape = (cfg.hyper.heads, cfg.hyper.latent_dim, cfg.hyper.latent_dim // cfg.hyper.heads)
+    shape = (cfg.heads, cfg.latent_dim, cfg.latent_dim // cfg.heads)
     q, k, v = init_array(keys[0], shape), init_array(keys[1], shape), init_array(keys[2], shape)  # type: ignore
-    p = init_array(keys[3], (cfg.hyper.latent_dim, cfg.hyper.latent_dim))  # type: ignore
+    p = init_array(keys[3], (cfg.latent_dim, cfg.latent_dim))  # type: ignore
     return Attention(q=q, k=k, v=v, p=p)
 
 
 def init_ffwd_fn(rng: Array, cfg: Conf) -> Feedforward:
-    w1 = init_array(rng, (cfg.hyper.latent_dim, cfg.hyper.latent_dim * 4))  # type: ignore
-    w2 = init_array(rng, (cfg.hyper.latent_dim * 4, cfg.hyper.latent_dim))  # type: ignore
+    w1 = init_array(rng, (cfg.latent_dim, cfg.latent_dim * 4))  # type: ignore
+    w2 = init_array(rng, (cfg.latent_dim * 4, cfg.latent_dim))  # type: ignore
     return Feedforward(w1=w1, w2=w2)
 
 
@@ -152,9 +152,9 @@ def init_block(cfg: Conf, rng: jnp.ndarray) -> Block:
 
 
 def init_fn(rng: Array, cfg: Conf):  # x: Array, y: Array) -> mi.types.Params:
-    keys = random.split(rng, 2 + cfg.hyper.depth)
+    keys = random.split(rng, 2 + cfg.depth)
     embeds = init_embed_fn(keys[0], cfg)
-    unbeds = init_array(keys[1], (cfg.hyper.latent_dim, y_fn(cfg)))  # type: ignore
+    unbeds = init_array(keys[1], (cfg.latent_dim, y_fn(cfg)))  # type: ignore
     blocks = lax.map(partial(init_block, cfg), keys[2:])
     return Params(embeds=embeds, unbeds=unbeds, blocks=blocks)
 
@@ -163,9 +163,9 @@ def init_fn(rng: Array, cfg: Conf):  # x: Array, y: Array) -> mi.types.Params:
 def y_fn(cfg: Conf) -> int:  # infers the number of tasks we are solving
     primes = jnp.array(A000040[1 : cfg.prime**2 * 2])
     primes = primes[primes < jnp.sqrt(cfg.prime**2)]
-    tasks = primes.shape[0] + 1  #  if cfg.task == "prime" else cfg.vocab_size
+    tasks = primes.shape[0] + 1  #  if cfg.project == "prime" else cfg.vocab_size
     # TODO: adapt to work with prose
-    return tasks if cfg.task == "miiii" else cfg.prime  # if task is nanda we wanna guess the mod
+    return tasks if cfg.project == "miiii" else cfg.prime  # if project is nanda we wanna guess the mod
 
 
 def dropout_fn(key: Array, x: Array, dropout: float) -> Array:
