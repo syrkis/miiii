9747d8e313f252231597bceb41a5cc5dd1a120e0
Noah Syrkis <noah@syrkis.com>
2024-11-10 17:15:13 +0100

    luden

--- ludens.py
+++ ludens.py
@@ -4,16 +4,27 @@
 
 
 # %% Imports
-import miiii as mi
 from aim import Repo
 import os
+import pickle
 
 
-
+# %%
 hash = "09411cac9a174bb3a95db629"
-hash_run_dir = os.path.join(os.getcwd(), "data/artifacts", hash)
-os.makedirs(hash_run_dir, exist_ok=True)
-repo = Repo('aim://localhost:53800')
-run = repo.get_run(hash)
-run.artifacts["metrics.pkl"].download(os.path.join(hash_run_dir, "metrics.pkl"))  # type: ignore
-run.artifacts["params.pkl"].download(os.path.join(hash_run_dir, "params.pkl"))  # type: ignore
+
+
+def get_metrics_and_params(hash):
+    hash_run_dir = os.path.join(os.getcwd(), "data/artifacts", hash)
+    os.makedirs(hash_run_dir, exist_ok=True)
+    repo = Repo("aim://localhost:53800")
+    run = repo.get_run(hash)
+    outs = {"state": None, "metrics": None, "acts": None}
+    for thing in ["matrics", "state", "acts"]:
+        run.artifacts[f"{thing}.pkl"].download(os.path.join(hash_run_dir))  # type: ignore
+        with open(os.path.join(hash_run_dir, f"{thing}.pkl"), "rb") as f:
+            outs[thing] = pickle.load(f)
+    return outs["state"], (outs["metrics"], outs["acts"])
+
+
+# %%
+state, (metrics, acts) = get_metrics_and_params(hash)
