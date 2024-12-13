b4f990056621bbfcd4a019d5def79aa4a77da60a
Noah Syrkis <noah@syrkis.com>
2024-12-13 23:04:34 +0100

    search space

--- main.py
+++ main.py
@@ -5,16 +5,11 @@
 # Imports
 import miiii as mi
 
-from jax import random
-
+arg = mi.utils.arg_fn()
+study = mi.optim.run_study(arg)
 
 # %% Setup
-arg = mi.utils.arg_fn()
 # rng, *keys = random.split(random.PRNGKey(0), 3)
 # ds = mi.tasks.task_fn(keys[0], cfg, arg)
-
-
-# %%
-study = mi.optim.run_study(arg)
 # state, (metrics, loss) = mi.train.train_fn(rng, cfg, arg, ds)
 # mi.utils.log_fn(cfg, arg, ds, state, metrics)
