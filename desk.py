0ea005da03ecbd63c02d41a64cb52a07ace3def5
Noah Syrkis <noah@syrkis.com>
2025-08-27 10:53:42 +0200

    codeberg

--- desk.py
+++ desk.py
@@ -11,4 +11,15 @@ res = pd.DataFrame(reader.filter(query_string=query))
 plt.plot(res["train.loss"].iloc[-1])
 
 # %%
-plt.plot(np.array(res["scope.valid_acc"].iloc[-1]).T.flatten())
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+axes[0].plot(np.array(res["scope.train_cce"].iloc[-1]), label=np.arange(15))
+axes[1].plot(np.array(res["scope.train_acc"].iloc[-1])))
+plt.legend()
+plt.show()
+
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+axes[0].plot(np.array(res["scope.valid_cce"].iloc[-1]), label=np.arange(15))
+axes[0].plot(np.array(res["scope.valid_acc"].iloc[-1]), label=np.arange(15))
+plt.legend()
+plt.show()
