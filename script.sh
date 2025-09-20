7166cb20c2c7c5d88274f3d08f289714d32873dd
Noah Syrkis <noah@syrkis.com>
2025-09-20 15:39:38 +0200

    WIP

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@
 
 # Run your Python script
 export XLA_PYTHON_CLIENT_MEM_FRACTION="1.00"
-time singularity exec --nv miiii.sif uv run python main.py device="hpc" p=113 epochs=65536 d=256 tick=128
+time singularity exec --nv miiii.sif uv run python main.py p=113 epochs=65536 d=256 tick=128
