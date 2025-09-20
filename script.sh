8462ca4569bff7fb138744d3ea43234242bcbac5
Noah Syrkis <noah@syrkis.com>
2025-09-20 16:40:59 +0200

    mlxp

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=noah
-#SBATCH --output=outs/job.%j.out      # Name of output file (%j expands to jobId)
+# #SBATCH --job-name=noah
+# #SBATCH --output=outs/job.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --time=01:00:00
 #SBATCH --gres=gpu:v100:1
 # #SBATCH --gpus=6
-#sbatch --mem=32G
+# #sbatch --mem=32G
 
 # Run your Python script
 export XLA_PYTHON_CLIENT_MEM_FRACTION="1.00"
